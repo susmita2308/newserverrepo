509807db5b16d6e644412facacd036d6db0a9d92
susmita2308 <susmitapawar1234@gmail.com>
2020-11-10 11:25:37 +0530

    server update

--- app.js
+++ app.js
@@ -1,13 +1,23 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const port = process.env.PORT || 1234;
-
-
+const cors = require("cors");
+require("./models/db");
 const app = express();
+const port = process.env.PORT ||1234;
+ app.use(cors());
+// app.use((req, res, next) => {
+//   res.header("Access-Control-Allow-Headers", "Authorization");
+//   res.header("Access-Control-Expose-Headers", "x-token");
+//   next();
+// });
+//middlewares
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use("/api/user", require("./routes/user.routes"));
+// app.use("/api/task", require("./routes/task.route"));
+// app.use("/api/auth", require("./routes/auth.route"));
+//  starting server
 
-app.use('/api/user',require('./routes/user.routes'));
 app.listen(port, () => {
-    console.log(`server is listen on ${port}`);
-  });
+  console.log(`server is listen on ${port}`);
+});
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -5,11 +5,10 @@ const UserCtrl = require("../controllers/user.controller");
 const router = express.Router();
 
 router.post("/", UserCtrl.insert);
-// router.post("/auth", UserCtrl.authenticate);
+// router.post("/auth", UserCtrl.security);
 // router.put("/:id", UserCtrl.update);
 // router.delete("/:id", UserCtrl.delete);
 // router.get("/:id", UserCtrl.getOne);
 // router.get("/", UserCtrl.getAll);
-//  export router missleware
 
 module.exports = router;
